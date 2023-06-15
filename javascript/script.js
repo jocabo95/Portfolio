44d57f85a47ce183ff8282d3ac73324f177ffbff
jocabo95 <camargojl1995@gmail.com>
2023-06-15 17:45:36 -0500

    initial footer animation

--- javascript/script.js
+++ javascript/script.js
@@ -18,6 +18,24 @@ function dropDownBackground () {
     navBarEl.classList.add("navScroll");
 }
 
+/*Change footer text color when bottom is reached*/
 
+let footerText = document.getElementById("footer-text");
+let hollaButton = document.querySelector(".holla-button");
 
+let docHeight = document.documentElement.scrollHeight;
+let windowHeight = window.innerHeight;
 
+window.addEventListener("scroll", ()=>{
+    
+    let scrollPosition = window.scrollY + windowHeight;
+
+    if(scrollPosition >= docHeight){
+        footerText.classList.add("footer-text-colored");
+        hollaButton.classList.add("holla-button-colored");
+    }
+    else{
+        footerText.classList.remove("footer-text-colored");
+        hollaButton.classList.remove("holla-button-colored");
+    }
+})
